f06036d81aee18c5ef655d6627bc496597a116cd
Yannick Ongena <yannick.ongena@gmail.com>
2020-04-29 09:42:13 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+
+
+data = pd.read_csv("dataset/wdbc.data",header=None)
+data.head()
+
+features = data.iloc[:,2:].values
+label = data.iloc[:,1].values
+
+
+labelEncoder_Y = LabelEncoder()
+label = labelEncoder_Y.fit_transform(label)
+
+
+trainFeatures, testFeatures, trainLabel, testLabel = train_test_split(features, label, test_size = 0.25, random_state = 0)
+
+
+sc = StandardScaler()
+trainFeatures = sc.fit_transform(trainFeatures)
+testFeatures = sc.transform(testFeatures)
+
+
+classifier = LogisticRegression(solver="liblinear")
+ou = classifier.fit(trainFeatures, trainLabel)
+print(ou)
+
+Y_pred = classifier.predict(testFeatures)
+
+cm = confusion_matrix(testLabel, Y_pred)
+print(cm)
